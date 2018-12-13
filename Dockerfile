e9a291130de49b6811144b9b59a6d134ffe9eb5a
balbino <balbiino@live.com>
2018-12-13 17:24:30 -0200

    Ajustes Compose

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
 FROM node:10.13-alpine
+
+LABEL maintainer "balbino.silva@gmail.com"
+
+# HEALTHCHECK --interval=5s \
+#             --timeout=5s \
+#             CMD curl -f http://127.0.0.1:3000 || exit 1
+
 ENV NODE_ENV production
 WORKDIR /usr/src/app
 COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict';
+require('dotenv').load();
 
 const express = require('express');
 const responseTime = require('response-time');
@@ -8,8 +9,15 @@ const client = redis.createClient(process.env.REDIS_URL);
 
 var app = express();
 
-//Cria um middleware que adiciona um X-Response-Time no header para as respostas
 app.use(responseTime());
+
+client.on('connect', function () {
+    console.log('Redis client conectado com sucesso!');
+});
+
+client.on('error', function (err) {
+    console.log('Houve um erro com Redis: ', err.message)
+});
 
 const getBook = (req, res) => {
     let isbn = req.query.isbn;
@@ -18,14 +26,16 @@ const getBook = (req, res) => {
     axios.get(url)
         .then(response => {
             let book = response.data.items;
-            // Configura a chave:isbn no cache. Com ele o conteudo do cache : titulo
-            // Configura tempo de expiração do cache para 1 hora (60 min)
-            client.setex(isbn, 3600, JSON.stringify(book));
+            let book_data = JSON.stringify(book);
 
+            if (!book_data) 
+                throw new Error('O livro que você esta procurando não foi encontrado !!!');
+
+            client.setex(isbn, 3600, book_data);
             res.send(book);
         })
         .catch(err => {
-            res.send('O livro que você esta procurando não foi encontrado !!!');
+            res.send(err.message);
         });
 };
 
@@ -34,6 +44,7 @@ const getCache = (req, res) => {
     let isbn = req.query.isbn;
     // Checa os dados do cache do servidor redis
     client.get(isbn, (err, result) => {
+        if (err) throw err;
         if (result) {
             res.send(result);
         } else {
@@ -46,6 +57,6 @@ const getCache = (req, res) => {
 app.get('/book', getCache);
 
 app.listen(3000, function () {
-    console.log('Node esta rodando na porta 3000 !!!');
+    console.log('Aplicação esta rodando na porta 3000.');
 });
 
